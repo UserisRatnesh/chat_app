1da671cf43c6eb8fa31aec3e7e5f6461fd6d5c5c
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-02-16 01:16:04 +0530

    Signup route working fine.

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -5,13 +5,18 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        message: "All feilds are required",
+      });
+    }
     if (password.length < 6) {
       return res
         .status(400)
         .json({ message: "Password must be at least 6 characters" });
     }
 
-    const user = await User.findOne(email);
+    const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "Email already exists" });
     }
@@ -34,17 +39,17 @@ export const signup = async (req, res) => {
       generateToken(newUser._id, res);
       await newUser.save();
 
-      res.status(201).json({
+      return res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
     } else {
-      res.status(400).json({ message: "Invalid creating new user" });
+      return res.status(400).json({ message: "Invalid creating new user" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Internal error" });
+    return res.status(500).json({ message: "Internal error" });
   }
 };
 
