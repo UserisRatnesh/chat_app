5a6890bf76202be6b184d6938e1f84115c02ab97
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-02-16 01:00:55 +0530

    Initially commit, Implemented till signup route.

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,56 @@
+import { generateToken } from "../lib/util.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne(email);
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash password using bcryptjs
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // You do not need to save to create the id
+    // Id is created just after the creation of the user
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // Generate JWT token here and send to cookie
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid creating new user" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal error" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+export const logout = (req, res) => {
+  res.send("logout route");
+};
