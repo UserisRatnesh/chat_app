25169eb2d99c8d3333827589d6484d44c03fc765
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-02-18 14:56:07 +0530

    Profile pic udpation route done.

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/util.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -98,5 +99,32 @@ export const logout = async (req, res) => {
     return res.status(500).json({
       message: "Internal server error",
     });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true },
+    );
+
+    res
+      .status(200)
+      .json({ message: "Prfile pic updated successfully", updatedUser });
+  } catch (error) {
+    console.log("Error in update profile : ", error.message);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signup, login, logout } from "../controller/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +14,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+// NOTE: Update profile picture
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
