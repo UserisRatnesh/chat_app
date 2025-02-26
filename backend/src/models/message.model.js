5963871639798affa232171d11174dfa21f1bf07
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-02-27 00:24:47 +0530

    Solved one bug in message model causing backend crash.

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
   },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+  {
+    timestamps: true,
   },
-  text: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
+);
 
-  timestamps: true,
-});
-
-const Message = mongoose.Model("Message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
