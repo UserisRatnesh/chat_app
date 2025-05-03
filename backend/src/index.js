37b23a31786433a0d1ee65aaf788ef8dc7341e81
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-05-03 18:47:48 +0530

    Changes made for production.

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +25,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`server running on port PORT ${PORT}`);
